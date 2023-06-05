baccc7d3f2d51da6a7b037bf5353951a78ce0df5
Nevshune <kangdon03@gmail.com>
2023-06-05 14:58:51 +0900

    20230605_3 git commit -m 20230605_3

--- src/KakaoMap.jsx
+++ src/KakaoMap.jsx
@@ -6,11 +6,11 @@ export default function KakaoMap() {
         const mapContainer = document.getElementById('map');
         const mapOption = {
             center: new window.kakao.maps.LatLng(35.9001012, 128.6377214), // 지도의 중심좌표
-            level: 5 // 지도의 확대 레벨
+            level: 4 // 지도의 확대 레벨
         };
         const Map = new window.kakao.maps.Map(mapContainer, mapOption);
 
-        const markerPosition = new window.kakao.maps.LatLng(35.9001012, 128.6377214);
+        const markerPosition = new window.kakao.maps.LatLng(35.9000500, 128.6371800);
 
         // 마커를 생성합니다
         const marker = new window.kakao.maps.Marker({
