20b445c2eebf674b64dee9eef1ba0164a9ff19d7
Nevshune <kangdon03@gmail.com>
2023-06-05 16:19:18 +0900

    20230605_5?

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,12 +1,49 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Button, Input, Radio, RadioGroup, Stack, Text, VStack } from "@chakra-ui/react";
 import Layout from "./Layout";
 
 export default function Contact() {
+
+
     return (
         <Layout>
-            <Box py={'140px'}>
-                <Text>연락처</Text>
-            </Box>
+            <VStack bg="blue.50" py="140px" w="full" alignItems={"flex-start"} px="4" spacing="10">
+
+                <Text fontWeight={700} fontSize={20}>
+                    무료상담신청
+                </Text>
+
+                <VStack as="form" w="full" alignItems={"flex-start"}>
+                    <Text fontWeight={600}>이름</Text>
+                    <Input placeholder="이름을 입력해 주세요" bg="white"></Input>
+                </VStack>
+
+                <VStack as="form" w="full" alignItems={"flex-start"}>
+                    <Text fontWeight={600}>연락처</Text>
+                    <Input  placeholder="연락처를 입력해 주세요 ( '-' 제외 )" bg="white"></Input>
+                </VStack>
+
+                <VStack as="form" w="full" alignItems={"flex-start"}>
+                    <Text fontWeight={600}>email</Text>
+                    <Input placeholder="이메일을 입력해 주세요" bg="white"></Input>
+                </VStack>
+
+                <VStack as="form" w="full" alignItems={"flex-start"}>
+                    <Text fontWeight={600}>참가 신청 연령</Text>
+                    <RadioGroup>
+                        <Stack>
+                            <Radio value='1' bg="white">성인(만 18세 이상)</Radio>
+                            <Radio value='2' bg="white">중·고등학생</Radio>
+                            <Radio value='3' bg="white">어린이(만 12세 미만)</Radio>
+                        </Stack>
+                    </RadioGroup>
+
+                </VStack>
+                <Button alignSelf={"center"} bg="gray.400" color={"white"}>문의하기</Button>
+
+
+
+            </VStack>
+
         </Layout>
     )
 }
--- src/profile.jsx
+++ src/profile.jsx
@@ -1,12 +1,25 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Avatar, Button, HStack, Text, VStack } from "@chakra-ui/react";
 import Layout from "./Layout";
 
 export default function Profile() {
     return (
         <Layout>
-            <Box py={'140px'}>
-                <Text>프로필</Text>
-            </Box>
+            <VStack bg="blue.50" minH="100vh" py="140px" w="full" alignItems={"center"} px="4" spacing="10">
+                <VStack>
+                    <Avatar name='Kent Dodds' size="2xl" src="https://images.unsplash.com/photo-1684919557234-df00bece5153?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" />
+                    <Text fontSize={20} fontWeight={600}>Rahul Kangdon Manish</Text>
+                    <Text fontSize={14}>풀스택 개발자를 꿈꾸는 개발지망생</Text>
+                </VStack>
+                {/* <HStack w="80%" spacing={"4"}>
+                    <Box border="2px" rounded={"xl"} px="20px" py="1" w="140px" Align="Center" color="blue.400" bg="white" fontWeight={500}>Contact</Box>
+                    <Box border="2px" rounded={"xl"} px="20px" py="1" w="140px" Align="Center" color="blue.400" bg="white" fontWeight={500}>Fallow</Box>
+                </HStack> */}
+                <HStack w="80%" spacing={"4"}>
+                <Button border="2px" rounded={"xl"} px="20px" py="1" w="140px" Align="Center" color="blue.400" bg="white" fontWeight={500}>Contact</Button>
+                <Button border="2px" rounded={"xl"} px="20px" py="1" w="140px" Align="Center" color="blue.400" bg="white" fontWeight={500}>Fallow</Button>
+                </HStack>
+
+            </VStack>
         </Layout>
     )
 }
