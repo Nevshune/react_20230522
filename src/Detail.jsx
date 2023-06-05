d77ade79d9120ca94c1237a60e08adc819bc4fbd
Nevshune <kangdon03@gmail.com>
2023-06-05 14:27:33 +0900

    20230605_2

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,5 +1,6 @@
 import { Box, Image, VStack, Text } from "@chakra-ui/react";
 import Layout from "./Layout";
+import KakaoMap from "./KakaoMap";
 
 
 export default function Detail() {
@@ -15,18 +16,25 @@ export default function Detail() {
                 </VStack>
                 <Box w="25%" h="2px" bg="gray.700"></Box>
                 <VStack w="full" alignItems={"flex-start"} px={4} spacing={1}>
+                    <Text color="white" fontWeight={600} > 신청 기간 </Text>
+                    <Text color="white">2023.10.31 18:00 예약 마감</Text>
+                </VStack>
+                <VStack w="full" alignItems={"flex-start"} px={4} spacing={1}>
                     <Text color="white" fontWeight={600} > 투어 일정 </Text>
                     <Text color="white">2023.11.25 ~ 2023.12.2 (7박 8일)</Text>
                 </VStack>
                 <Box w="25%" h="2px" bg="gray.700"></Box>
                 <VStack w="full" alignItems={"flex-start"} px={4} spacing={1}>
                     <Text color="white" fontWeight={600} > 가격 안내 </Text>
-                    <Text color="white">1,750,000원 (항공운임 포함)</Text>
+                    <Text color="white">2,750,000원 (항공/숙식 포함)</Text>
                 </VStack>
-                <Box w="25%" h="2px" bg="gray.700"></Box><VStack w="full" alignItems={"flex-start"} px={4} spacing={1}>
-                    <Text color="white" fontWeight={600} > 운영시간 </Text>
-                    <Text color="white">10:00 ~ 17:00</Text>
+                <Box w="25%" h="2px" bg="gray.700"></Box>
+                
+                <VStack w="full" alignItems={"flex-start"} px={4} spacing={1}>
+                    <Text color="white" fontWeight={600} > 출발 및 도착 장소 </Text>   
+                    <Text color="white">대구 국제 공항 3번 게이트</Text>                 
                 </VStack>
+                <KakaoMap />
                 <Box w="25%" h="2px" bg="gray.700"></Box>
 
 
---
+++ src/KakaoMap.jsx
@@ -0,0 +1,25 @@
+import { Box } from "@chakra-ui/react";
+import { useEffect } from "react";
+
+export default function KakaoMap() {
+    useEffect(() => {
+        const mapContainer = document.getElementById('map');
+        const mapOption = {
+            center: new window.kakao.maps.LatLng(35.9001012, 128.6377214), // 지도의 중심좌표
+            level: 5 // 지도의 확대 레벨
+        };
+        const Map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+        const markerPosition = new window.kakao.maps.LatLng(35.9001012, 128.6377214);
+
+        // 마커를 생성합니다
+        const marker = new window.kakao.maps.Marker({
+            position: markerPosition
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(Map)
+    }, [])
+
+    return <Box as="div" w="full" h="300px" id="map"></Box>
+}
